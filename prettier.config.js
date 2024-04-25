232d91964f2df32aefd963fb097752a3984f3294
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-04-25 11:36:59 +0200

    feat: add automatic class wrapping

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,15 @@
 /** @type {import("prettier").Config} */
+const functions = ["cn", "clsx"];
 const config = {
-  tailwindFunctions: ["cn", "clsx"],
-  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: functions,
+  customFunctions: functions,
+  endingPosition: "absolute-with-indent",
+  plugins: [
+    // https://www.nikolailehbr.ink/blog/tailwind-css-tips#Automatic-wrapping-of-long-class-names
+    "prettier-plugin-classnames",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-merge",
+  ],
   semi: true,
   tabWidth: 2,
   singleQuote: false,
