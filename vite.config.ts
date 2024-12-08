00790b29e19281c27e9ec1903e058ebfe190f50c
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-12-08 19:02:03 +0100

    chore(vite): remove script flag for server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    host: true,
+    open: true,
+  },
   plugins: [
     remix({
       future: {
