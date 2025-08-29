e6f353ce9d5ef6e72baad9b116b1582db6e9a8d8
Nikolai Lehbrink <mail@nikolailehbr.ink>
2025-08-29 12:47:06 +0200

    fix: simplify Vite configuration by removing noExternal settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ command }) => ({
+export default {
   server: {
     open: true,
     host: true,
   },
-  ssr: {
-    noExternal: command === "build" ? true : undefined,
-  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-}));
+} satisfies UserConfig;
